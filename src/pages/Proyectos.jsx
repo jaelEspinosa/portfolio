aec65344f95ab5286ad7486c4a57dbdb35acee40
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2022-08-09 13:04:54 +0200

    add proyectos

--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -6,10 +6,12 @@ import deployment from '../img/deployment.png'
 import weather from '../img/weather.jpg'
 import guitarLa from '../img/guitarLa.jpg'
 import quiosco from '../img/quiosco.jpg'
+import uptask from '../img/upTask.jpg'
 
 const Proyectos = () => {
   return (
     <div className={styles.contain}>
+     
       <article className={styles.proyecto}>
         <h2 className={styles.title}>Game of Thrones</h2>
         <div className={styles.cardContain}>
@@ -19,6 +21,7 @@ const Proyectos = () => {
               Web basada en la serie. Se puede consultar el nombre y casa de
               cada uno de los personajes de la serie. 
             </p>
+            <p>(Sólo vista Ordenador)</p>
             <p className={styles.resumen}>Tecnologia: React scss </p>
             <p className={styles.resumen}>
               librerias: React-router-dom, axios,
@@ -38,7 +41,7 @@ const Proyectos = () => {
           <img className={styles.imagen} src={weather} alt="weather-app" />
           <div className={styles.description}>
             <p className={styles.resumen}>
-              App meterológica. Si das permisos de ubicación, consulta a la api wheather y renderiza al predicción meterológica en las horas y dias próximos.
+              App meteorológica. Si das permisos de ubicación, consulta a la api wheather y renderiza al predicción meteorológica en las horas y dias próximos.
               También tiene un buscador para encontrar la ciudad deseada y consultar el clima de ésta.
             </p>
             <p className={styles.resumen}>Tecnologia: React css </p>
@@ -63,7 +66,7 @@ const Proyectos = () => {
               Emulación Ecommerce. Se trata de una tienda de guitarras y cursos de guitarra.
             </p>
             <p className={styles.resumen}>Tecnologia: NextJs css </p>
-            <p className={styles.resumen}>
+            <p className={styles.resumen}>backend: strapi
               
               
             </p>
@@ -76,6 +79,30 @@ const Proyectos = () => {
         
       </article>
       <article className={styles.proyecto}>
+        <h2 className={styles.title}>UpTask Mern</h2>
+        <div className={styles.cardContain}>
+          <img className={styles.imagen} src={uptask} alt="game-of-thrones" />
+          <div className={styles.description}>
+            <p className={styles.resumen}>
+              Crea y gestiona proyectos y tareas. 
+            </p>
+            <p></p>
+            <p className={styles.resumen}>Front: React css </p>
+            <p className={styles.resumen}>
+              librerias: React-router-dom, axios, tailwind y socketIo.
+            </p>
+            <p className={styles.resumen}>back: Nodejs <a href="https://github.com/jaelEspinosa/uptask_backend.git" target='_blank'><img className={styles.rrssLittle}src={git} alt='github'/></a>  </p>
+            <p className={styles.resumen}>
+              librerias: express, nodemailer, mongoose y socketIo.
+            </p>
+            <div className={styles.rrssContain}>
+               <a href="https://github.com/jaelEspinosa/uptasck_frontend.git" target='_blank'><img className={styles.rrss}src={git} alt='github'/></a> 
+               <a href='https://uptask-mern.netlify.app/' target='_blank'><img className={styles.rrss}src={deployment} alt='github'/></a>  
+            </div>
+          </div>
+        </div>
+      </article>
+      <article className={styles.proyecto}>
         <h2 className={styles.title}>Quiosco App</h2>
         <div className={styles.cardContain}>
           <img className={styles.imagen} src={quiosco} alt="guitarLa-app" />
@@ -83,8 +110,8 @@ const Proyectos = () => {
             <p className={styles.resumen}>
               Aplicación de quiosco para cafeteria. Es una app para gestionar pedidos de bebida o comida.
             </p>
-            <p className={styles.resumen}>Tecnologia: NextJs, Prisma tailwind </p>
-            <p className={styles.resumen}>libreias: toastify, react-modal
+            <p className={styles.resumen}>Tecnologia: NextJs</p>
+            <p className={styles.resumen}>libreias: toastify, react-modal, Prisma y tailwind 
               
               
             </p>
