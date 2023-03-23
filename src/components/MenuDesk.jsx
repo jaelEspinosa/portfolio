e5148fe605ccaa1a94e1304ba4bb89b51b4fb5ed
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2023-03-23 17:29:30 +0100

    styles

--- src/components/MenuDesk.jsx
+++ src/components/MenuDesk.jsx
@@ -10,16 +10,20 @@ const MenuDesk = () => {
     <nav>
       <ul className={styles.desk}>
         <Link to={'/'} ><li style={{
-          textDecoration: location.pathname === '/' ? 'underline red 5px' : 'none'
+          textDecoration: location.pathname === '/' ? 'underline red 5px' : 'none',
+          transition: '0.8s'
         }} className={styles.itemdesk}>Inicio</li></Link>
         <Link to={'/about'}><li style={{
-          textDecoration: location.pathname === '/about' ? 'underline red 5px' : 'none'
+          textDecoration: location.pathname === '/about' ? 'underline red 5px' : 'none',
+          transition: '0.8s'
         }} className={styles.itemdesk}>Acerca de mi</li></Link>
         <Link to={'/proyectos'}><li style={{
-          textDecoration: location.pathname === '/proyectos' ? 'underline red 5px' : 'none'
+          textDecoration: location.pathname === '/proyectos' ? 'underline red 5px' : 'none',
+          transition: '0.8s'
         }} className={styles.itemdesk}>Proyectos</li></Link>
         <Link to={'/contacto'}><li style={{
-          textDecoration: location.pathname === '/contacto' ? 'underline red 5px' : 'none'
+          textDecoration: location.pathname === '/contacto' ? 'underline red 5px' : 'none',
+          transition: '0.8s'
         }} className={styles.itemdesk}>Contacto</li></Link> 
       </ul>
     </nav>
